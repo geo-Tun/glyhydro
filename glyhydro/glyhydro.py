bda03d7c4a77a98e5f90132cd405fc40eb7a0c68
Oluwatunmise <hardeybayooluwatunmise@gmail.com>
2026-05-12 15:41:16 +0100

    Update glyhydro.py

--- glyhydro/glyhydro.py
+++ glyhydro/glyhydro.py
@@ -1,1 +1,5 @@
 """Main module."""
+def SCS_T_lag(hydraulic_length, max_retention_post_runoff, Curve_Number, average_watershed_slope):
+        S= (1000/Curve_Number)-10
+        T_lag= ((hydraulic_length)**0.8)* ((S + 1)**0.7)/(1900*(sqrt(average_watershed_slope)))
+        return T_lag
