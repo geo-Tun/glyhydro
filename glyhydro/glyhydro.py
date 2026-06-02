d00292ea03e2f0e0e6a935d96b212ed2177b78bf
Oluwatunmise <hardeybayooluwatunmise@gmail.com>
2026-06-02 14:20:46 +0100

    Update glyhydro.py

--- glyhydro/glyhydro.py
+++ glyhydro/glyhydro.py
@@ -6,4 +6,21 @@ def SCS_T_lag(hydraulic_length, max_retention_post_runoff, Curve_Number, average
 if _name__= "__main__":
     SCS_T_lag()
     
-
+def latitude(degree, minutes, seconds, hemisphere=""):
+    coord= float(degree+ (minutes/60)+(seconds/3600))
+    hemisphere = input("Enter the Hemispere")
+    new_hem ="N", "North", "north", "Northern", "n", "S", "s", "South", "south","Southern"
+    if hemisphere not in new_hem:
+        return "Not the expected hemisphere for latitude"
+    else:
+        return str(coord) +  hemisphere.upper()
+    
+def longitude(degree, minutes, seconds, hemisphere=""):
+    coord= float(degree+ (minutes/60)+(seconds/3600))
+    hemisphere = input("Enter the Hemispere")
+    new_hem ="E", "East", "east", "eastern", "w", "W", "e", "West", "western","Western"
+    if hemisphere not in new_hem:
+        return "Not the expected hemisphere for longitude"
+    else:
+        return str(coord) +  hemisphere.upper()
+  
